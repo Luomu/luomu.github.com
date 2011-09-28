81d4e131fec04093f2bac600190943d8c5623986
Kimmo Kotajärvi <kimmo@paahdin.com>
2011-09-28 19:20:30 -0400

    Shaded sphere

--- sphere.js
+++ sphere.js
@@ -9,13 +9,43 @@ function log(txt) {
 	console.log(txt);
 }
 
+requestAnimFrame = window.mozRequestAnimationFrame;
+
+var renderer;
+var camera;
+var scene;
+
 function init() {
+
+var vsSource = "varying vec3 vNormal;\
+	void main() \
+	{\
+		vNormal = normal;\
+		gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);\
+	}"
+
+var uniforms = {
+	fade: {
+		type: 'f',
+		value: 0.5
+	}
+};
+
+var fsSource = "varying vec3 vNormal;\
+	uniform float fade;\
+	void main()\
+	{\
+		vec3 light = normalize(vec3(0.5,0.2,1.0));\
+		float dProd = max(0.0, dot(vNormal, light));\
+		vec3 color = vec3(fade, fade, fade) * dProd;\
+		gl_FragColor = vec4(color.r, color.g, color.b, 1.0);\
+	}"
 
 var $container = $('#container');
 
-var renderer = new THREE.WebGLRenderer();
-var camera = new THREE.Camera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-var scene = new THREE.Scene();
+renderer = new THREE.WebGLRenderer();
+camera = new THREE.Camera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+scene = new THREE.Scene();
 
 camera.position.z = 300;
 
@@ -24,13 +54,26 @@ renderer.setSize(WIDTH, HEIGHT);
 $container.append(renderer.domElement);
 
 	var radius = 50, segments = 16, rings = 16;
-	var sphereMaterial = new THREE.MeshLambertMaterial(
-		{ color: 0xcc0000 });
+	/*var sphereMaterial = new THREE.MeshLambertMaterial(
+		{ color: 0xcc0000 });*/
+	scene.sphereMaterial = new THREE.MeshShaderMaterial({
+		uniforms: 		uniforms,
+		vertexShader: 	vsSource,
+		fragmentShader:	fsSource
+	});
 	var sphere = new THREE.Mesh(
 		new THREE.SphereGeometry(radius, segments, rings),
-		sphereMaterial);
-	
+		scene.sphereMaterial);
+
 	scene.addChild(sphere);
-	
+
+	requestAnimFrame(update);
+}
+
+var frame = 0;
+function update() {
+	scene.sphereMaterial.uniforms.fade.value = Math.cos(frame);
+	frame += 0.1;
 	renderer.render(scene, camera);
+	requestAnimFrame(update);
 }
